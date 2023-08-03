a25cbbcab3615f8cae881b81d35efd342fcd4729
Daniel-Jantrambun <daniel.jantrambun@pm.me>
2023-08-03 08:42:40 +0200

    fix errors

--- heroku-certificate-from-k8s-cert-manager.go
+++ heroku-certificate-from-k8s-cert-manager.go
@@ -4,17 +4,17 @@ import (
 	"context"
 	"crypto/x509"
 	"encoding/pem"
+	"errors"
 	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
-	"runtime"
 	"time"
-	"errors"
 
-	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 
 	heroku "github.com/heroku/heroku-go/v5"
@@ -48,29 +48,35 @@ func main() {
 		os.Exit(1)
 	}
 
-	kubernetesTlsExpiresOn, err := getKubernetesCertificateExpirationDate(k8sCertificate.CertificateChain)
+	kubernetesTlsExpiresOn, err := getCertificateExpirationDate(k8sCertificate.CertificateChain)
 	if err != nil {
 		fmt.Printf("error getting kubernetes tls expires on: %v\n", err)
 		os.Exit(1)
 	}
 	fmt.Printf("kubernetes TLS expiresOn %v\n", kubernetesTlsExpiresOn)
-
-	sni, err := getHerokuSni()
+	kubernetesTlsCommonName, err := getCertificateCommonName(k8sCertificate.CertificateChain)
 	if err != nil {
-		fmt.Printf("error getting kubernetes tls expires on: %v\n", err)
+		fmt.Printf("error getting kubernetes tls common nam: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("kubernetes TLS common name %v\n", kubernetesTlsCommonName)
+
+	sni, err := getHerokuSni(kubernetesTlsCommonName)
+	if err != nil {
+		fmt.Printf("error getting Heroku SNI: %v\n", err)
 		os.Exit(1)
 	}
 
 	if (sni != nil) {
 		herokuCertificateExpiresAt := sni.SSLCert.ExpiresAt
-		fmt.Printf("kubernetes TLS expires at %v\n", herokuCertificateExpiresAt)
+		fmt.Printf("Heroku TLS expires at %v\n", herokuCertificateExpiresAt)
 
 		if (*kubernetesTlsExpiresOn != sni.SSLCert.ExpiresAt) {
 			fmt.Println("both dates are not the same")
 			uploadCertificateToHeroku(&k8sCertificate, sni.Name)
+		} else {
+			fmt.Println("both dates are identical. No need to update the heroku certificate.")
 		}
-
-		fmt.Println("both dates are identical. No need to update the heroku certificate.")
 	} else {
 		fmt.Println("no SNI. Will install one")
 		createCertificateToHeroku(&k8sCertificate)
@@ -107,30 +113,46 @@ func initVariables() error {
 	return nil
 }
 
-func getKubernetesCertificates() (k8sCertificate k8sCertificate, err error) {
-	userHomeDir, err := os.UserHomeDir()
+func getClientset() (clientset *kubernetes.Clientset, err error) {
+	// automountServiceAccountToken must not be set to false in kubernetes pod
+	config, err := rest.InClusterConfig()
 	if err != nil {
-		fmt.Printf("error getting user home dir: %v\n", err)
-		return
-	}
-	os := runtime.GOOS
-	var kubeConfigPath string
-    switch os {
-		case "windows":
-		case "darwin":
-			kubeConfigPath = filepath.Join(userHomeDir, ".kube", "config")
-			fmt.Printf("Using kubeconfig: %s\n", kubeConfigPath)
+		fmt.Println("Using out-of-cluster mode")
+		userHomeDir, err := os.UserHomeDir()
+		if err != nil {
+			fmt.Printf("error getting user home dir: %v\n", err)
+			return nil, err
 		}
-
-	kubeConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
-	if err != nil {
-		fmt.Printf("Error getting kubernetes config: %v\n", err)
-		return
+		kubeConfigPath := filepath.Join(userHomeDir, ".kube", "config")
+		fmt.Printf("Using kubeConfig: %s\n", kubeConfigPath)
+		
+		kubeConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
+		if err != nil {
+			fmt.Printf("Error getting kubernetes config: %v\n", err)
+			return nil, err
+		}
+		clientset, err = kubernetes.NewForConfig(kubeConfig)
+		if err != nil {
+			fmt.Printf("error getting kubernetes client out cluster: %v\n", err)
+			return nil, err
+		}
+		return clientset, err
+	} else {	
+		fmt.Println("Using in-of-cluster mode")
+		clientset, err = kubernetes.NewForConfig(config)
+		if err != nil {
+			fmt.Printf("error getting kubernetes client inCluster: %v\n", err)
+			return nil, err
+		}
 	}
 
-	clientset, err := kubernetes.NewForConfig(kubeConfig)
+	return clientset, err
+}
+
+func getKubernetesCertificates() (k8sCertificate k8sCertificate, err error) {
+	clientset, err := getClientset()
 	if err != nil {
-		fmt.Printf("error getting kubernetes config: %v\n", err)
+		fmt.Printf("error getting kubernetes client: %v\n", err)
 		return
 	}
 
@@ -147,19 +169,30 @@ func getKubernetesCertificates() (k8sCertificate k8sCertificate, err error) {
 	return
 }
 
-func getKubernetesCertificateExpirationDate(crt string) (kubernetesTlsExpiresOn *time.Time, err error) {
+func getCertificateExpirationDate(crt string) (kubernetesTlsExpiresOn *time.Time, err error) {
 	block, _ := pem.Decode([]byte(crt))
 	certificate, err := x509.ParseCertificate(block.Bytes)
 	if err != nil {
 		fmt.Printf("error parsing block certificate: %v\n", err)
 		return
 	}
-	fmt.Printf("kubernetes certificate expires on: %v\n", certificate.NotAfter)
 	kubernetesTlsExpiresOn = &certificate.NotAfter
 	return
 }
 
-func getHerokuSni() (herokuSniEndpoint *heroku.SniEndpoint, err error) {
+func getCertificateCommonName(crt string) (kubernetesTlsCommonName string, err error) {
+	block, _ := pem.Decode([]byte(crt))
+	certificate, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		fmt.Printf("error parsing block certificate: %v\n", err)
+		return
+	}
+	kubernetesTlsCommonName = certificate.Subject.CommonName
+
+	return
+}
+
+func getHerokuSni(commonName string) (herokuSniEndpoint *heroku.SniEndpoint, err error) {
 	heroku.DefaultTransport.BearerToken = *herokuToken
 	var app *heroku.App
 	h := heroku.NewService(heroku.DefaultClient)
@@ -168,23 +201,28 @@ func getHerokuSni() (herokuSniEndpoint *heroku.SniEndpoint, err error) {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(app.Name)
-	fmt.Println(app.CreatedAt)
+	fmt.Printf("Heroku app name %v\n", app.Name)
 
 	var sniResult heroku.SniEndpointListResult
 	var lr *heroku.ListRange
 	sniResult, err = h.SniEndpointList(context.TODO(), *herokuApp, lr)
 	if err != nil {
-		fmt.Println("no sni endpoint list")
+		fmt.Println("Heroku : No sni endpoint list")
 		fmt.Println(err)
 		return
 	}
 	for _, sni := range sniResult {
-		fmt.Println(sni.Name)
-		fmt.Println(sni.Domains)
-		fmt.Println(sni.CreatedAt)
-		fmt.Println(sni.SSLCert.ExpiresAt)
-		herokuSniEndpoint = &sni
+		fmt.Printf("Heroku Sni Name: %v\n", sni.Name)
+		sniCommonName, err := getCertificateCommonName(sni.CertificateChain)
+		if err != nil {
+			fmt.Printf("error getting sni tls common nam: %v\n", err)
+			return nil, err
+		}
+		fmt.Printf("sni common name: %v\n", sniCommonName)
+		if sniCommonName == commonName {
+			herokuSniEndpoint = &sni
+			return herokuSniEndpoint, nil
+		}
 	}
 	return
 }
